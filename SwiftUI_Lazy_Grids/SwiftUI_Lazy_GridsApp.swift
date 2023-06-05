028591057924f9501b55cc58efb2f4058722c762
Mladen Mikic <mladen.mikic@ignit.group>
2023-06-05 10:36:43 +0200

    Initial Commit

---
+++ SwiftUI_Lazy_Grids/SwiftUI_Lazy_GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Lazy_GridsApp.swift
+//  SwiftUI_Lazy_Grids
+//
+//  Created by MladenMikic on 05.06.2023..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Lazy_GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
